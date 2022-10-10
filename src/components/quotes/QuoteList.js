b7cb7d0facebd48e3c340880f45c82cf54718b67
Chinmay Joshi <chinmay.joshi@excellarate.com>
2022-10-10 15:30:31 +0530

    Added the UI for filter

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,22 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
 const QuoteList = (props) => {
+  const history= useHistory()
+  const location=useLocation()
+ const queryparams= new URLSearchParams(location.search)
+ const isSortAscending=queryparams.get('sort')==='asc'
+  const sortHandler=()=>{
+   history.push('/quotes?sort='+(isSortAscending ? 'desc':'asc'))
+  }
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={sortHandler}>Sort {isSortAscending ? 'Ascending' : 'Descending'}</button>
+        </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
